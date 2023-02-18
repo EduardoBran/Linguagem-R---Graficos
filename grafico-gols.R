41952cff851a1d608802c2909b68c11ea93573e9
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-02-18 13:21:22 -0300

    gerando grafico dos maiores artilheiros do futebol - parte 1

---
+++ grafico-gols.R
@@ -0,0 +1,11 @@
+# Gráfico com maiores goleadores de 2016 a 2021
+
+# Configurando Diretorio de Trabalho
+setwd("C:/Users/Julia/Desktop/CienciaDeDados/1.Big-Data-Analytics-com-R-e-Microsoft-Azure-Machine-Learning/3.Linguagem-R-Fatores-Estruturas-de-Controle-e-Funcoes")
+getwd()
+
+
+
+jogadores <- c('L. Messi', 'C. Ronaldo', 'L. Suarez', 'R. Lewandowski', 'Z. Ibrahimovic')
+gols <- c(404, 399, 284, 273, 250)
+ano <- c(2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)
