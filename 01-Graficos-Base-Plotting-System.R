6f12b9263b387831ef4cebf3517fe6117b2e5a16
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-02-24 15:21:46 -0300

    graficos - base plotting system - parte 2

--- 01-Graficos-Base-Plotting-System.R
+++ 01-Graficos-Base-Plotting-System.R
@@ -60,7 +60,7 @@ boxplot(Ozone ~ Month, airquality, xlab = 'Month', ylab = 'Ozone (ppb)')
 # col    - cor do plot
 # lty    - tipo de linha
 # lwd    - largura da linha
-# pch    - simbolo no plot
+# pch    - valor do simbolo no plot
 # xlab   - label no eixo x 
 # ylab   - label no eixo y
 # las    - como os labels dos eixos sao orientados
@@ -76,5 +76,102 @@ boxplot(Ozone ~ Month, airquality, xlab = 'Month', ylab = 'Ozone (ppb)')
 # text()   - adiciona label ao grafico
 # title()  - adiciona titulo ao grafico
 
-# caso queira fazer ajustes em dos parametros, temos a funcao par()
+# caso queira fazer consultas ou configurar um dos parametros, temos a funcao par()
+
 ?par
+par()
+par('pch')
+par('lty')
+par('bg')
+
+x = 2:4
+y = 1:8
+
+plot(x, pch = 'c') # alterando o valor do simbolo no grafico
+
+
+# O comando par(mfrow = c(2,2), col.axis = 'red') é usado em R para configurar o layout
+# dos gráficos em uma matriz 2x2 e definir a cor dos rótulos dos eixos como vermelha.
+
+# Explicando cada argumento:
+
+# - mfrow é um parâmetro que define o layout dos gráficos que serão criados. No caso de mfrow = c(2,2),
+#   isso significa que serão criados quatro gráficos, organizados em uma matriz de duas linhas e duas colunas.
+# - col.axis é um parâmetro que define a cor dos rótulos dos eixos dos gráficos.
+#   No caso de col.axis = 'red', a cor será vermelha.
+# Dessa forma, quando os gráficos forem criados usando funções como plot(),
+# eles serão organizados em uma matriz 2x2 e terão rótulos de eixos vermelhos.
+
+par(mfrow = c(2,2), col.axis = 'red') # para retirar essa cong basta clicar na vassoura na area de plotagem abaixo
+
+# Em resumo, o argumento las é usado para controlar a orientação dos rótulos dos eixos nos gráficos
+# produzidos pela função plot().
+
+plot(y, las = 0, xlab = 'xlab', ylab = 'ylab', main = 'LAS = 0')
+plot(y, las = 1, xlab = 'xlab', ylab = 'ylab', main = 'LAS = 1')
+plot(y, las = 2, xlab = 'xlab', ylab = 'ylab', main = 'LAS = 2')
+plot(y, las = 3, xlab = 'xlab', ylab = 'ylab', main = 'LAS = 3')
+
+legend('topright', pch = 1, col = c('blue', 'red'), legend = c('Var1', 'Var2')) # só entra no ultimo grafico
+
+par(mfrow = c(1,1)) # voltando a exibir apenas 1 grafico
+
+
+# Visualizando cores disponiveis das cores que podemos usar na linguagem r
+colors()
+
+
+
+# Salvando os graficos
+
+
+# png (quando chamado a funcao png(), ele nao exibe o grafico na area de plotagem e salva direto)
+
+# exemplo1 
+
+png('GraficoIris2.png', width = 1000, height = 1000, res = 100)
+
+plot(iris$Sepal.Length, iris$Petal.Length,
+     col = iris$Species,
+     main = 'Grafico gerado a partir de Iris')
+
+dev.off() # fecha o dispositivo de graficos
+
+
+# exemplo2 
+
+png('GraficoAltura.png', width = 1000, height = 1000, res = 100)
+
+times = c('Flamengo', 'Fluminense', 'Vasco', 'Botafogo', 'Avai')
+df = data.frame(Altura = c(1.78, 1.82, 1.98, 1.89, 1.69),
+                Peso = c(80, 92, 115, 95, 78),
+                Times = factor(times, levels = c('Flamengo', 'Fluminense', 'Vasco', 'Botafogo', 'Avai')))
+
+cores <- c('red', 'green4', 'black', 'gray', 'blue')
+
+plot(df$Altura, df$Peso,
+     col = cores, # col = df$Time escolhe as cores automaticamente
+     pch = 'O',
+     main = 'Grafico gerado com altura e peso da média dos times')
+
+legend('topleft',
+       legend = levels(df$Time),
+       col = cores,  # col = 1:length(levels(df$Time)) - escolhe cores automaticamente
+       pch = 'O')
+
+dev.off()
+
+
+# Salvando em pdf
+
+pdf('GraficoTimesPdf.pdf')
+
+plot(iris$Sepal.Length, iris$Petal.Length,
+     col = iris$Species,
+     main = 'Grafico gerado a partir de Iris')
+
+dev.off()
+
+
+
+# Estendendo as funcoes do plot
