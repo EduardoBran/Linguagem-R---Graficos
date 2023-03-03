b8a3c511a74d6ac35decabf530c9b162cd37c5f6
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-03-03 12:34:32 -0300

    explorando o ggplot2 - parte 2

--- 07-Explorando-o-ggplot2.R
+++ 07-Explorando-o-ggplot2.R
@@ -39,10 +39,10 @@ qplot(total_bill, tip, data = tips, geom = 'point', xlab = 'Valor da Conta', yla
 # Outro exemplo usando qplot()
 
 # criando df com valores direto na funcao
-df_valor_casas <- data.frame(Preco = c(572, 780, 859, 950),
-                              `N de Quartos` = c('2', '3', '4', '5'),
-                              Bairro = c('Classe Alta', 'Classe Media', 'Classe Alta', 'Classe Alta'),
-                              row.names = c('Casa 1', 'Casa 2', 'Casa 3', 'Casa 4'))
+df_valor_casas <- data.frame(Preco = c(572, 780, 859, 950, 450, 680, 980),
+                              `N de Quartos` = c('2', '3', '4', '5', '2', '3', '5'),
+                              Bairro = c('Classe Alta', 'Classe Media', 'Classe Alta', 'Classe Alta', 'Classe Media', 'Classe Media', 'Classe Alta'),
+                              row.names = c('Casa 1', 'Casa 2', 'Casa 3', 'Casa 4', 'Casa 5', 'Casa 6', 'Casa 7'))
 
 # criando o qplot
 qplot(df_valor_casas$Preco, df_valor_casas$N.de.Quartos, data = df_valor_casas,
@@ -74,9 +74,40 @@ ggplot(data = tips, aes(x = total_bill, y = tip, color = sex)) + # color diz qua
 ??aes
 
 
-# 
+# *** Construindo um modelo de regressao (modelo de regressao linear simples) ***
+
+# - Primeiro criamos um modelo regressao linear simples (modelo_base) utilizando a formula tip ~ total_bill que vai indicar
+#   que queremos prever a gorjeta com base no valor total da conta. O argumento data = tips especifica que os dados a serem usados sao do dataframe tips.
+
+# - Depois criamos um dataframe com duas colunas. A primeira coluna é chamada de "total_bill" e contém os valores 
+#   da variavel "total_bill" do conjunto de dados "tips".
+# - A segunda coluna é gerada pela função "predict" que serve para gerar as previsoes da variavel resposta ('tip') com
+#   base nos valores da variável preditora ("total_bill") usando modelo de regressão "modelo_base" e especificamos
+#   o intervalo de confiança de 95% com o argumento "interval = "confidence".
+# - O resultado é uma matriz com três colunas: os valores previstos para a variável resposta (fit),
+#   o limite inferior do intervalo de confiança (lwr) e o limite superior do intervalo de confiança (upr).
+# - Se o valor total da conta ('total_bill') foi de 10,34 ao lado teremos o valor medio que deve ser a gorgeta + valor alto + valor baixo
+
+modelo_base <- lm(tip ~ total_bill, data = tips) 
+summary(modelo_base)
+
+modelo_fit <- data.frame(                        
+  total_bill = tips$total_bill,
+  predict(modelo_base, interval = "confidence")
+)
+View(modelo_fit)
 
 
+# Camada 2
+# - esta criando um objeto de linha (geom_line) baseada nos dados do modelo_fit
+# - mapeia o eixo x (total_bill) e o eixo y (fit) para a linha que será criada com a cor 'darkred'
+
+camada2 <- geom_line(
+  mapping = aes(x = total_bill, y = fit), # fit vem do df modelo_fit
+  data = modelo_fit,
+  color = 'darkred'
+)
+ggplot() + camada1 + camada2
 
 
 
@@ -95,4 +126,97 @@ ggplot() + camada1_df
 # resumido
 ggplot(data = df_valor_casas, aes(x = df_valor_casas$Preco, y = df_valor_casas$N.de.Quartos, color = df_valor_casas$Bairro)) +
   geom_point(size = 3)
+
+
+# Construindo modelo de regressao linear simples (prevendo valor de acordo com numero de quartos)
+
+modelo_base2 <- lm(df_valor_casas$Preco ~ df_valor_casas$N.de.Quartos, data = df_valor_casas)
+summary(modelo_base2)
+
+modelo_fit2 <- data.frame(
+  `N de Quartos` = df_valor_casas$N.de.Quartos,
+  predict(modelo_base2, interval = "confidence")
+)
+View(modelo_fit2)
+
+modelo_fit2$log_fit <- log(modelo_fit2$fit) # necessario para a linha da regressao aparecer junto com os dados
+
+# Camada 2
+
+camada2_df <- geom_line(
+  mapping = aes(x = df_valor_casas$Preco, y = log_fit),
+  data = modelo_fit2,
+  color = "darkred"
+)
+ggplot() + camada1_df + camada2_df
+
+
+
+# *** Explicando a saida de ' summary (modelo_base) ' ***
+
+# Call:
+#   lm(formula = tip ~ total_bill, data = tips)
+# 
+# Residuals:
+#   Min      1Q  Median      3Q     Max 
+# -3.1982 -0.5652 -0.0974  0.4863  3.7434 
+# 
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept) 0.920270   0.159735   5.761 2.53e-08 ***
+#   total_bill  0.105025   0.007365  14.260  < 2e-16 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 1.022 on 242 degrees of freedom
+# Multiple R-squared:  0.4566,	Adjusted R-squared:  0.4544 
+# F-statistic: 203.4 on 1 and 242 DF,  p-value: < 2.2e-16
+
+
+# - A linha "Call:" indica que o modelo foi ajustado usando a função lm() com a fórmula "tip ~ total_bill" 
+#   e os dados do dataframe tips.
+
+# - A secao "Residuals:" mostra as estatísticas descritivas dos resíduos do modelo (diferença entre os valores observados 
+#   e os valores preditos pelo modelo). O mínimo, 1º quartil, mediana, 3º quartil e máximo dos resíduos são exibidos.
+
+# - A secao "Coefficients:" mostra os coeficientes do modelo, incluindo o intercepto e o coeficiente de inclinação para a
+#   variável preditora total_bill. Para cada coeficiente, temos sua estimativa, erro padrão, estatística t, e valor p.
+#   O intercepto é estimado em 0.920270, e o coeficiente para a variável total_bill é 0.105025. Ambos os coeficientes sao
+#   significantes no nível de significância de 0.05, como indicado pelos símbolos de asterisco ao lado dos valores p.
+
+#   * O intercepto é a estimativa do valor da variável de resposta quando a variável preditora é zero. No caso deste modelo, o 
+#   intercepto é de 0.920270, o que significa que quando a conta do cliente é zero, a gorjeta esperada é de aproximadamente 92 centavos.
+#   * O coeficiente de inclinação para a variável preditora (total_bill) mede a mudança esperada na variável de resposta (tip)
+#   quando a variavel preditora aumenta em uma unidade. Neste modelo, o coeficiente de inclinação é de 0.105025, o que 
+#   significa que a cada aumento de um dólar na conta do cliente, espera-se um aumento de 10,5 centavos na gorjeta.
+#   * O erro padrao e uma medida de quão preciso é a estimativa do coeficiente. Quanto menor o erro padrão, 
+#   mais confiável é a estimativa.
+#   * A estatística t mede a significância estatística do coeficiente. Quanto maior a estatística t, mais significativo é
+#   o coeficiente. Nesse modelo, tanto o intercepto quanto o coeficiente de inclinação são estatisticamente significativos,
+#   com estatísticas t de 5.761 e 14.260, respectivamente.
+#   * O valor p mede a probabilidade de obter uma estatística t tão grande ou maior do que a observada se a hipotese nula de
+#   que o coeficiente é zero fosse verdadeira. Um valor p menor que o nível de significância (geralmente 0,05) indica que é
+#   improvável que o coeficiente seja zero e que ele é estatisticamente significativo. Neste modelo, ambos os coeficientes
+#   têm valores p muito baixos, indicando que são altamente significativos.
+
+# - A secao "Residual standard error:" mostra o desvio padrão dos resíduos, que é uma medida da variabilidade nao
+#   explicada pelo modelo.
+
+# - A secao "Multiple R-squared:" mostra o coeficiente de determinação R², que é uma medida de quão bem o modelo se ajusta
+#   aos dados. Nesse caso, o modelo explica cerca de 45,66% da variabilidade na gorjeta.
+
+# - A secao "Adjusted R-squared:" mostra o R² ajustado, que leva em consideração o número de preditores no modelo.
+#   Nesse caso, o R² ajustado é ligeiramente menor que o R², indicando que o modelo pode estar sofrendo de superajuste.
+
+# - A secao "F-statistic:" mostra a estatistica F para testar se pelo menos um dos coeficientes é diferente de zero.
+#   Nesse caso, o valor de F é 203.4, indicando que há forte evidência de que pelo menos um dos coeficientes é diferente de zero.
+
+# - A secao "p-value:" mostra o valor p para a estatística F. O valor p é muito baixo, menor que 2.2e-16,
+#   indicando que há forte evidência contra a hipótese nula de que todos os coeficientes são zero.
+#   Isso confirma a conclusão da seção "F-statistic".
+
+
+png('GraficoDisperComModeloDeRegressao.png', width = 1500, height = 1000, res = 100)
+
+dev.off() # fecha o dispositivo de graficos
 
